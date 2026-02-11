425ce73a095c5cdca87cbb057f872a819de224c3
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-11 18:20:02 +0100

    feat(milestone 2): filtro di ricerca con useMemo

--- src/components/ListaPolitici.jsx
+++ src/components/ListaPolitici.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo} from "react";
 import CardPolitico from "./CardPolitico"
 
 function ListaPolitici() {
@@ -6,36 +6,61 @@ function ListaPolitici() {
     /***********
         HOOK
     ************/
+    const [politici, setPolitici] = useState([]);           // Hook di Stato che contiene la lista completa dei politici
+    const [ricerca, setRicerca] = useState("")              // Hook di Stato che salva il valore dell'input di ricerca dell'utente
 
-    // Hook di Stato che contienee una lista di politici
-    const [politici, setPolitici] = useState([]);
+    // Hook di Memo che calcola l'array filtrato dei politici
+    const politiciFiltrati = useMemo(() => {                
 
-    // Hook di Effetto che recupera una lista di politici al mounting
+        // Input vuoto --> restituisce tutti i politici
+        if (ricerca === "") {
+            return politici;
+        }
+
+        // Input pieno --> restituisce i politici filtrati
+        return (
+            politici.filter(politico =>
+                politico.name.toLowerCase().includes(ricerca.toLowerCase()) ||
+                politico.biography.toLowerCase().includes(ricerca.toLowerCase())
+            )
+        );
+    }, [ricerca, politici])
+
+    // Hook di Effetto che recupera una lista politici
     useEffect(() => {
         fetchPolitici();
     }, []);
+    
 
     /************
         RENDER
     *************/
     return(
             <section className="sezione-politici">
-                    <h1> Lista Politici </h1>
-                    <ul>
-                        {politici.map(politico => (
-                            <li key={politico.id}>
-                                <CardPolitico
-                                    politico = {politico}
-                                />
-                            </li>
-                        ))}
-                    </ul>
+                <h1> Lista Politici </h1>
+                <input 
+                    type="text"
+                    placeholder="Inserisci nome o biografia"
+                    value = {ricerca}
+                    onChange = { e => setRicerca(e.target.value) }
+                />
+                <ul>
+                    {politiciFiltrati.map(politicoFiltrato => (
+                        <li key={politicoFiltrato.id}>
+                            <CardPolitico
+                                politico={politicoFiltrato}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </section>
     )
 
     /**************
         FUNZIONI
     ***************/
+
+    // Funzione che recupera una lista di politici 
     async function fetchPolitici() {
         try {
             const response = await fetch('http://localhost:3333/politicians')
